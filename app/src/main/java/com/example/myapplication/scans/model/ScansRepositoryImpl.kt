0d995dd99fc46fa5ae1d421b1fb9d96796becf75
Ioan <ioan.cretan_ext@autoscout24.com>
2023-04-19 12:26:37 +0300

    add unit tests

--- app/src/main/java/com/example/myapplication/scans/model/ScansRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/scans/model/ScansRepositoryImpl.kt
@@ -2,12 +2,18 @@ package com.example.myapplication.scans.model
 
 class ScansRepositoryImpl : ScansRepository {
 
-    override suspend fun getScans(): MutableList<ScanItem> {
+    @Throws(Exception::class)
+    override suspend fun getScans() : MutableList<ScanItem>{
         val items = (1..10).map {
             ScanItem(
                 it
             )
         }
+
+        if (items.size == 0) {
+            throw Exception("not valid")
+        }
+
         return items.toMutableList()
     }
 }
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,17 +1,86 @@
 package com.example.myapplication
 
-import org.junit.Test
-
+import com.example.myapplication.scans.model.ScanItem
+import com.example.myapplication.scans.model.ScansRepositoryImpl
+import com.example.myapplication.scans.model.ScansUseCase
+import com.example.myapplication.scans.viewModel.ScansViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.stub
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
+    @InjectMocks
+    lateinit var scansUseCase: ScansUseCase
+
+    @Mock
+    lateinit var repository: ScansRepositoryImpl
+
+    val dispatcher = TestCoroutineDispatcher()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun setScansToState_WHEN_repoReturnScansWithSucces() = runTest {
+
+        val scansToReturn = getMockItems()
+
+        repository.stub {
+            onBlocking { getScans() }.doReturn(scansToReturn)
+        }
+
+        val scansViewModel = ScansViewModel(scansUseCase)
+        assertEquals(scansToReturn, scansViewModel.state.value.scans.value)
+    }
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun setErrorToState_WHEN_repoReturnScansWithError() = runTest {
+
+        repository.stub {
+            onBlocking { getScans() }.doThrow(Exception(ERROR_MSG))
+        }
+
+        val scansViewModel = ScansViewModel(scansUseCase)
+        assertEquals(ERROR_MSG, scansViewModel.state.value.error)
+    }
+
+    private fun getMockItems(): MutableList<ScanItem> {
+        val items = (1..10).map {
+            ScanItem(
+                it
+            )
+        }
+        return items.toMutableList()
+    }
+
+    companion object {
+        private const val ERROR_MSG = "not valid"
     }
 }
